277beb0e9a233c506510c90652d8de6e88d5e39c
j003918 <j003918@foxmail.com>
2017-01-06 10:36:48 +0800

    add vba sql script

---
+++ sql/oracle_comm.sql
@@ -0,0 +1,107 @@
+CREATE OR REPLACE FUNCTION PARSE_FEE_CHINESE(N_FEE IN NUMBER) 
+RETURN VARCHAR2 AS
+  V_CHINESE_NUMBER VARCHAR2(30)  := '零壹贰叁肆伍陆柒捌玖';
+  V_CHINESE_POINT  VARCHAR2(30)  := '分角元拾佰仟万亿';
+  V_RESULT         VARCHAR2(100) := '';
+  V_TEMP           VARCHAR2(100);
+  V_FLAG           VARCHAR2(10);
+  V_MAIN           VARCHAR2(40);
+  V_SUB            VARCHAR2(20);
+  N_TEMPFEE        NUMBER(30, 2);
+  C_CH1            CHAR(1);
+  C_CH2            VARCHAR2(10);
+  N_LENGTH         NUMBER(10) := 0;
+  I                NUMBER(10) := 0;
+  J                NUMBER(10) := 0;
+  T                NUMBER(10) := 0;
+BEGIN
+  IF N_FEE     < 0 THEN
+    V_FLAG    := '负';
+    N_TEMPFEE := -1 * N_FEE;
+  ELSE
+    N_TEMPFEE := N_FEE;
+    V_FLAG    := '';
+  END IF;
+  SELECT
+    TRIM(TO_CHAR(ROUND(N_TEMPFEE, 2), '9999999999999999999.99'))
+  INTO
+    V_TEMP
+  FROM
+    DUAL;
+  IF N_FEE    = 0 THEN
+    V_RESULT := '零元整';
+  ELSE
+    --先处理整数，再处理小数
+    V_MAIN     := SUBSTR(V_TEMP, 1, INSTR(V_TEMP, '.') - 1);
+    V_SUB      := SUBSTR(V_TEMP, INSTR(V_TEMP, '.')    + 1);
+    N_LENGTH   := LENGTH(V_MAIN);
+    I          := N_LENGTH;
+    T          := 0;
+    IF V_MAIN  IS NULL OR '0' = V_MAIN THEN
+      V_RESULT := '零' || V_RESULT;
+    END IF;
+    WHILE I > 0
+    LOOP
+      I            := I                - 1;
+      T            := T                + 1;
+      C_CH1        := SUBSTR(V_MAIN, I + 1);
+      V_MAIN       := SUBSTR(V_MAIN, 1, I);
+      J            := TO_NUMBER(C_CH1);
+      IF T          = 5 OR t= 13 THEN
+        V_RESULT   := '万' || V_RESULT;
+      ELSIF T       = 9 THEN
+        IF '万'      = SUBSTR(V_RESULT, 1, 1) THEN
+          V_RESULT := '亿' || SUBSTR(V_RESULT, 2);
+        ELSE
+          V_RESULT := '亿' || V_RESULT;
+        END IF;
+      END IF;
+      IF '0'          = C_CH1 THEN
+        IF I         <> N_LENGTH AND '零' <> C_CH2 THEN
+          IF T       <> 5 AND T <> 9 AND T <> 13 THEN
+            V_RESULT := '零' || V_RESULT;
+          ELSIF T     = 5 OR T = 9 OR t=13 THEN
+            V_RESULT := SUBSTR(V_RESULT, 1, 1) || '零' ||
+            SUBSTR(V_RESULT, 2);
+          END IF;
+          C_CH2 := SUBSTR(V_CHINESE_NUMBER, J, 1);
+        END IF;
+      ELSE
+        J       := MOD(T, 4);
+        IF T    <> 5 AND T <> 9 AND T <> 13 THEN
+          IF J  <> 1 THEN
+            IF J = 0 THEN
+              J := J + 4;
+            END IF;
+            J        := J + 2;
+            V_RESULT := SUBSTR(V_CHINESE_POINT, J, 1) || V_RESULT;
+          END IF;
+        END IF;
+        J        := TO_NUMBER(C_CH1) + 1;
+        C_CH2    := SUBSTR(V_CHINESE_NUMBER, J, 1);
+        V_RESULT := C_CH2 || V_RESULT;
+      END IF;
+    END LOOP;
+    V_RESULT   := V_RESULT || '元';
+    IF V_SUB   IS NULL OR LENGTH(TRIM(V_SUB)) <= 0 OR TO_NUMBER(V_SUB) = 0 THEN
+      V_RESULT := V_RESULT || '整';
+    ELSE
+      C_CH1      := SUBSTR(V_SUB, 1, 1);
+      J          := TO_NUMBER(C_CH1) + 1;
+      C_CH2      := SUBSTR(V_CHINESE_NUMBER, J, 1);
+      IF '0'      = C_CH1 THEN
+        V_RESULT := V_RESULT || C_CH2 ;
+      ELSE
+        V_RESULT := V_RESULT || C_CH2 || '角';
+      END IF;
+      C_CH1      := SUBSTR(V_SUB, 2, 1);
+      IF '0'     <> C_CH1 THEN
+        J        := TO_NUMBER(C_CH1) + 1;
+        C_CH2    := SUBSTR(V_CHINESE_NUMBER, J, 1);
+        V_RESULT := V_RESULT || C_CH2 || '分';
+      END IF;
+    END IF;
+  END IF;
+  V_RESULT := V_FLAG || V_RESULT;
+  RETURN V_RESULT;
+END;
---
+++ vba/comm.vbs
@@ -0,0 +1,54 @@
+Public Function j_sqlstr(str, btrim As Boolean)
+    If btrim = False Then
+        j_sqlstr = "'" & str & "'"
+    Else
+        j_sqlstr = "'" & Trim(str) & "'"
+    End If
+End Function
+
+
+Public Function insert_sql(tbname As String, btrim As Boolean, ParamArray col())
+'
+'µ÷ÓÃ =insert_sql("taname",true,$B$2:$E$2,$B$3:$E$3,$C3:$E3)
+'
+'
+   insert_sql = "insert into " & Trim(tbname) & " ("
+    str_tmp = ""
+    col_names = col(0)
+    col_types = col(1)
+    col_vals = col(2)
+    
+   
+    Dim col_nums, i As Integer
+    i = 0
+    col_nums = 0
+
+    For Each col_name In col_names
+        str_tmp = str_tmp & "," & Trim(col_name)
+        col_nums = col_nums + 1
+    Next
+    str_tmp = str_tmp & ") values ("
+    
+    insert_sql = insert_sql & Mid(str_tmp, 2)
+    str_tmp = ""
+    
+    Dim Arr() As String
+    ReDim Arr(col_nums + 1)
+    
+    For Each col_type In col_types
+        Arr(i) = col_type
+        i = i + 1
+    Next
+    
+    i = 0
+    For Each col_val In col_vals
+        If Arr(i) = "str" Then
+            str_tmp = str_tmp & "," & j_sqlstr(col_val, btrim)
+        Else
+            str_tmp = str_tmp & "," & Trim(col_val)
+        End If
+        i = i + 1
+    Next
+       
+    insert_sql = insert_sql & Mid(str_tmp, 2) & ");"
+End Function
