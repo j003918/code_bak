c91046952fa32847adbe9453d80769d09ab9cb59
unknown <>
2017-03-28 15:02:19 +0800

    modify hide to tray

--- delphi/TrayNotify/Main.pas
+++ delphi/TrayNotify/Main.pas
@@ -17,8 +17,14 @@ type
     procedure N1Click(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure TrayIcon1Click(Sender: TObject);
+    procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
   private
+    procedure WMSysCommand(var Msg: TWMSysCommand); message WM_SYSCOMMAND;
     procedure SetTrayFlash(IsFlash : Boolean);
+    procedure HideToTray;
+    procedure ShowToDesktop;
     { Private declarations }
   public
     { Public declarations }
@@ -31,21 +37,63 @@ implementation
 
 {$R *.dfm}
 
+procedure TfrmMain.HideToTray;
+begin
+    Application.Minimize;
+    ShowWindow(Application.Handle, SW_HIDE);
+end;
+procedure TfrmMain.ShowToDesktop;
+begin
+    ShowWindow(Application.Handle, SW_RESTORE);
+end;
+
+procedure TfrmMain.WMSysCommand(var Msg: TWMSysCommand);
+begin
+  inherited;
+  if Msg.CmdType = SC_MINIMIZE then
+  begin
+    HideToTray;
+  end;
+end;
 
 procedure TfrmMain.SetTrayFlash(IsFlash: Boolean);
 begin
+
+  if TrayIcon1.Animate = IsFlash then exit;
+
   TrayIcon1.Animate := IsFlash;
-  if not IsFlash then TrayIcon1.IconIndex := 0
+  if IsFlash then trayicon1.ShowBalloonHint;
+  if not IsFlash then TrayIcon1.IconIndex := 0;
+end;
+
+procedure TfrmMain.TrayIcon1Click(Sender: TObject);
+begin
+  ShowToDesktop;
 end;
 
 procedure TfrmMain.Button1Click(Sender: TObject);
 begin
-       SetTrayFlash(true)
+  SetTrayFlash(true);
 end;
 
 procedure TfrmMain.Button2Click(Sender: TObject);
 begin
       SetTrayFlash(false)
+end;
+
+procedure TfrmMain.FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+begin
+  HideToTray;
+  CanClose:=False;
+end;
+
+procedure TfrmMain.FormShow(Sender: TObject);
+var
+Style: Integer;
+begin
+Style := GetWindowLong(Handle, GWL_EXSTYLE);
+SetWindowLong(Handle, GWL_EXSTYLE, Style and (not WS_EX_APPWINDOW));
+ShowWindow(Application.Handle, SW_HIDE);
 end;
 
 procedure TfrmMain.N1Click(Sender: TObject);
--- delphi/TrayNotify/TrayNotify.dpr
+++ delphi/TrayNotify/TrayNotify.dpr
@@ -1,5 +1,7 @@
 program TrayNotify;
 
+{$R *.dres}
+
 uses
   Forms,
   Main in 'Main.pas' {frmMain},
@@ -10,22 +12,27 @@ uses
 
 begin
   Application.Initialize;
-  Application.MainFormOnTaskbar := True;
-
+  //Application.MainFormOnTaskbar := True;
+  Application.MainFormOnTaskbar := False;
+   {
   frmSplash := TfrmSplash.Create(Application);
   frmSplash.Show;
   frmSplash.Update;
 
   frmSplash.StatusText := '准备启动...';
   frmSplash.Update;
-
+    }
   try
     Application.CreateForm(TfrmMain, frmMain);
   finally
+    {
     frmSplash.StatusText := '完成启动...';
     frmSplash.Update;
     frmSplash.Close;
+    }
   end;
-
+  {
   if login.doLogin() then Application.Run;
+  }
+  Application.Run;
 end.
