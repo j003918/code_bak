5f7e11faddcbdaf8f743f5abfac7149621f38e98
unknown <>
2017-04-25 17:44:43 +0800

    add method

--- golang/project/src/test/main.go
+++ golang/project/src/test/main.go
@@ -7,28 +7,26 @@ import (
 )
 
 func Add(ctx context.Context, a, b int) int {
-	time.Sleep(3 * time.Second)
+	time.Sleep(5 * time.Second)
 	select {
 	case <-ctx.Done():
 		return -1
 	default:
-		fmt.Println("select")
 	}
 	return a + b
 }
 
 func main() {
-
 	{
 		a := 3
 		b := 6
-		timeout := 1 * time.Second
+		timeout := 6 * time.Second
 		ctx, _ := context.WithTimeout(context.Background(), timeout)
 		res := Add(ctx, a, b)
 		if nil != ctx.Err() {
 			fmt.Println(ctx.Err().Error())
 		}
-		fmt.Printf("Compute: %d+%d, result: %d\n", a, b, res)
+		fmt.Printf("%d+%d=%d\n", a, b, res)
 	}
 
 	{
@@ -43,6 +41,6 @@ func main() {
 		if nil != ctx.Err() {
 			fmt.Println(ctx.Err().Error())
 		}
-		fmt.Printf("Compute: %d+%d, result: %d\n", a, b, res)
+		fmt.Printf("%d+%d=%d\n", a, b, res)
 	}
 }
